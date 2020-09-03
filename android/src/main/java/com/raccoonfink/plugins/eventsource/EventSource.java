e3e9c641c716836455bd68d7e9e85acd149fcb24
Benjamin Reed <ranger@opennms.org>
2020-09-03 12:03:11 -0400

    switch to oksse version

--- android/src/main/java/com/raccoonfink/plugins/eventsource/EventSource.java
+++ android/src/main/java/com/raccoonfink/plugins/eventsource/EventSource.java
@@ -1,16 +1,23 @@
 package com.raccoonfink.plugins.eventsource;
 
+import android.util.Log;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.tylerjroach.eventsource.EventSourceHandler;
-import com.tylerjroach.eventsource.MessageEvent;
+import com.here.oksse.OkSse;
+import com.here.oksse.ServerSentEvent;
+
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import org.json.JSONObject;
+
+import androidx.annotation.WorkerThread;
+import okhttp3.Request;
+import okhttp3.Response;
 
 enum ReadyState {
     CONNECTING,
@@ -19,8 +26,10 @@ enum ReadyState {
 }
 
 @NativePlugin
-public class EventSource extends Plugin implements EventSourceHandler {
-    private com.tylerjroach.eventsource.EventSource eventSource;
+public class EventSource extends Plugin implements ServerSentEvent.Listener {
+    private static final String TAG = "EventSource";
+
+    private ServerSentEvent sse;
     private URL url;
     private boolean opened = false;
 
@@ -33,77 +42,114 @@ public class EventSource extends Plugin implements EventSourceHandler {
             return;
         }
 
-        System.err.println("configuring event source with URL " + urlString);
+        Log.i(TAG, "configuring event source with URL " + urlString);
 
         try {
             this.url = new URL(urlString);
         } catch (final MalformedURLException e) {
+            Log.w(TAG, "invalid URL", e);
             call.reject("invalid URL");
         }
 
-        if (this.eventSource != null) {
-            this.eventSource.close();
+        if (this.sse != null) {
+            Log.v(TAG, "configure() called, but there is an existing event source; making sure it's closed");
+            this.sse.close();
             this.opened = false;
-        }
-
-        try {
-            this.eventSource = new com.tylerjroach.eventsource.EventSource.Builder(this.url.toURI()).eventHandler(this).build();
-        } catch (final URISyntaxException e) {
-            call.reject("invalid URL");
         }
 
         call.resolve();
     }
 
+    @PluginMethod
     public void open(final PluginCall call) {
-        this.opened = true;
+        Log.i(TAG, "opening connection");
+
         final JSObject ret = new JSObject();
         ret.put("state", ReadyState.CONNECTING);
-        this.notifyListeners("onReadyStateChanged", ret);
-        this.eventSource.connect();
+        this.notifyListeners("onReadyStateChanged", ret, true);
+
+        final Request request = new Request.Builder().url(this.url).build();
+        final OkSse okSse = new OkSse();
+        this.sse = okSse.newServerSentEvent(request, this);
+
+        this.opened = true;
+
+        call.resolve();
     }
 
+    @PluginMethod
     public void close(final PluginCall call) {
-        if (this.eventSource != null && this.opened) {
-            this.eventSource.close();
+        Log.i(TAG, "closing connection");
+
+        if (this.sse != null && this.opened) {
+            this.sse.close();
             this.opened = false;
         }
+
+        call.resolve();
     }
 
     @Override
-    public void onConnect() throws Exception {
+    public void onOpen(final ServerSentEvent sse, final Response response) {
+        Log.v(TAG, "onOpen");
+
         final JSObject ret = new JSObject();
         ret.put("value", null);
-        this.notifyListeners("onOpen", ret);
+        this.notifyListeners("onOpen", ret, true);
 
         final JSObject rsRet = new JSObject();
         rsRet.put("state", ReadyState.OPEN);
-        this.notifyListeners("onReadyStateChanged", rsRet);
+        this.notifyListeners("onReadyStateChanged", rsRet, true);
     }
 
     @Override
-    public void onMessage(final String event, final MessageEvent message) throws Exception {
+    public void onMessage(final ServerSentEvent sse, final String id, final String event, final String message) {
+        Log.v(TAG, "onMessage: " + message);
+
         final JSObject ret = new JSObject();
-        ret.put("message", message.data);
-        this.notifyListeners("onMessage", ret);
+        ret.put("message", message);
+        this.notifyListeners("onMessage", ret, true);
     }
 
+    @WorkerThread
     @Override
-    public void onComment(final String comment) throws Exception {
-        System.err.println("unhandled event: onComment: " + comment);
+    public void onComment(final ServerSentEvent sse, final String comment) {
+        Log.v(TAG, "onComment: " + comment);
     }
 
+    @WorkerThread
     @Override
-    public void onError(final Throwable t) {
+    public Request onPreRetry(final ServerSentEvent sse, final Request originalRequest) {
+        Log.v(TAG, "onPreRetry");
+        return originalRequest;
+    }
+
+    @WorkerThread
+    @Override
+    public boolean onRetryTime(final ServerSentEvent sse, final long milliseconds) {
+        Log.v(TAG, "onRetryTime: " + milliseconds);
+        return true; // True to use the new retry time received by SSE
+    }
+
+    @WorkerThread
+    @Override
+    public boolean onRetryError(final ServerSentEvent sse, final Throwable throwable, final Response response) {
+        Log.w(TAG, "onRetryError: " + throwable.getMessage());
+
         final JSObject ret = new JSObject();
-        ret.put("error", t.getMessage());
-        this.notifyListeners("onError", ret);
+        ret.put("error", throwable.getMessage());
+        this.notifyListeners("onError", ret, true);
+
+        return true; // True to retry, false otherwise
     }
 
+    @WorkerThread
     @Override
-    public void onClosed(final boolean willReconnect) {
+    public void onClosed(final ServerSentEvent sse) {
+        Log.v(TAG, "onClosed");
+
         final JSObject ret = new JSObject();
         ret.put("state", ReadyState.CLOSED);
-        this.notifyListeners("onReadyStateChanged", ret);
+        this.notifyListeners("onReadyStateChanged", ret, true);
     }
 }
